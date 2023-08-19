e1cda16124c4717283ec22b70fb5e7be9f7aca50
Alina Chasova <a.chasova@innopolis.ru>
2023-08-19 15:51:57 +0400

    Add Force to the Player

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 300f;
+    public float sideForce = 200f;
+
+    // Update is called once per frame
+    // FixedUpdate is used for physics
+    void FixedUpdate()
+    {
+        // Object moves forward
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            // Object moves sideways
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            // Object moves sideways
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
